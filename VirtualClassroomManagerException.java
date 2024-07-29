492f6722b1b88c1bb6f5b29b4c29352736b8ee4f
priyasha1808 <147809315+priyasha1808@users.noreply.github.com>
2024-07-29 13:22:06 +0530

    Add files via upload
    Run the main file

---
+++ VirtualClassroomManagerException.java
@@ -0,0 +1,7 @@
+package com.virtualclassroom;
+
+public class VirtualClassroomManagerException extends Exception {
+    public VirtualClassroomManagerException(String message) {
+        super(message);
+    }
+}
